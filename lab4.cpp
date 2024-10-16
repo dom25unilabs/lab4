222bb56bed743efbfdb4c89e67e4c03a65539623
dom25b <dom250506@gmail.com>
2024-10-17 00:01:07 +0300

    fixes as required

--- lab4.cpp
+++ lab4.cpp
@@ -16,6 +16,21 @@ static void mul_mat(const int n, const int m, const int k, const double* const m
 		}
 	}
 }
+static void pow_mat(const int n,const int k, const double* const mat, double* const res1, double* const res2)
+{
+	mul_mat(n, n, n, mat, mat, res1);
+	for (int i = 1; i < k - 1; i++)
+	{
+		if (i % 2)
+		{
+			mul_mat(n, n, n, res1, mat, res2);
+		}
+		else
+		{
+			mul_mat(n, n, n, res2, mat, res1);
+		}
+	}
+}
 static void out_mat(const int n, const int m, const double* const mat)
 {
 	int maxlen = INT32_MIN;
@@ -59,6 +74,13 @@ static void out_mat_autoui(const int n, const int m, const double* const mat)
 		std::wcout << mat[i * m + m - 1] << '\n';
 	}
 }
+static void in_mat(const int n, const int m, double* const mat) 
+{
+	for (int i = 0; i < n * m; i++)
+	{
+		std::wcin >> mat[i];
+	}
+}
 int wmain(int argc, wchar_t* argv[])
 {
 	if (!(_setmode(_fileno(stdout), _O_U8TEXT) && _setmode(_fileno(stdin), _O_U8TEXT) && _setmode(_fileno(stderr), _O_U8TEXT)))
@@ -71,10 +93,7 @@ int wmain(int argc, wchar_t* argv[])
 	int n, m;
 	std::wcin >> n >> m;
 	double* a1 = new double[n * m];
-	for (int i = 0; i < n * m; i++)
-	{
-		std::wcin >> a1[i];
-	}
+	in_mat(n, m, a1);
 	int cmd = 1, k = 0;
 	while (cmd != 0)
 	{
@@ -100,10 +119,7 @@ int wmain(int argc, wchar_t* argv[])
 			std::wcin >> k;
 			double* a2 = new double[m * k];
 			double* res = new double[n * k] {};
-			for (int i = 0; i < m * k; i++)
-			{
-				std::wcin >> a2[i];
-			}
+			in_mat(m, k, a2);
 			mul_mat(n, m, k, a1, a2, res);
 			delete[] a1;
 			a1 = res;
@@ -118,26 +134,17 @@ int wmain(int argc, wchar_t* argv[])
 				std::wcin >> k;
 				double* res1 = new double[n * n] {};
 				double* res2 = new double[n * n] {};
-				mul_mat(n, n, n, a1, a1, res1);
-				for (int i = 1; i < k - 1; i++)
-				{
-					if (i % 2)
-					{
-						mul_mat(n, n, n, res1, a1, res2);
-					}
-					else
-					{
-						mul_mat(n, n, n, res2, a1, res1);
-					}
-				}
+				pow_mat(n,k,a1,res1,res2);
 				delete[] a1;
 				if (k % 2)
 				{
 					a1 = res2;
+					delete[] res1;
 				}
 				else
 				{
 					a1 = res1;
+					delete[] res2;
 				}
 			}
 			else
